eeebd6c7fac963c13d72fcb871d994d7ca349989
Mersiha Karamustafic <karamustafic.mersiha@gmail.com>
2024-03-04 23:59:30 +0100

    feat: prints sender data

--- app.py
+++ app.py
@@ -23,6 +23,16 @@ def extract_payment_date(text):
 
     return re.findall(dateRegex, text)
 
+def extract_sender_data(thresh):
+    details = pytesseract.image_to_data(thresh, output_type=pytesseract.Output.DICT, config='--oem 3 --psm 4')
+
+    # extract sender's data within particular range
+
+    cropped_image = image[820:1070, 160:1720]
+    # Perform OCR on the extracted region using pytesseract
+    extracted_text = pytesseract.image_to_string(cropped_image)
+    print(extracted_text)
+
 
 file_path = 'C:/Users/PcCentar/Desktop/repos/bank_statements_ocr/bank_statements/report.pdf'
 
@@ -35,7 +45,6 @@ image = np.array(image)
 # PREPROCESSING
 
 # Resize the image to improve OCR accuracy and speed
-# You can adjust the size based on your requirements
 image = cv2.resize(image, None, fx=2, fy=2, interpolation=cv2.INTER_CUBIC)
 
 # Convert the image to grayscale
@@ -44,13 +53,18 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 # Apply Gaussian blur to reduce noise and improve accuracy
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
+plt.imshow(image)
+plt.show()
 
 extractedText = pytesseract.image_to_string(thresh, config='--oem 3 --psm 4')
 orderNumber = extract_order_number(extractedText)
 amount = extract_amount(extractedText)
 paymentDate = extract_payment_date(extractedText)
+sender_data = extract_sender_data(thresh)
 
-print(extractedText)
+
+#print(extractedText)
 print(orderNumber)
 print(amount)
 print(paymentDate)
+#print(sender_data)
