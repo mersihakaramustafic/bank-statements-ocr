d15da7a785958089ea15be3bfbb872f627495c54
Mersiha Karamustafic <karamustafic.mersiha@gmail.com>
2024-03-10 23:54:39 +0100

    feat: updates constants file; adds amount, purpose of remittance, payment date values

--- constants.py
+++ constants.py
@@ -19,6 +19,13 @@ receiver = {
     'end_y': 1900
 }
 
+purpose_of_remittance = {
+    'start_x': 160,
+    'start_y': 1240,
+    'end_x': 1720,
+    'end_y': 1470
+}
+
 sender_bank_account = {
     'start_x': 2360,
     'start_y': 750,
@@ -31,4 +38,18 @@ receiver_bank_account = {
     'start_y': 910,
     'end_x': 3040,
     'end_y': 990
+}
+
+amount = {
+    'start_x': 2350,
+    'start_y': 1180,
+    'end_x': 3100,
+    'end_y': 1270
+}
+
+payment_date = {
+    'start_x': 620,
+    'start_y': 2120,
+    'end_x': 940,
+    'end_y': 2200
 }
