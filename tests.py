8968c63d596bcf680bb3c79bcf675d8547642f16
Mersiha Karamustafic <karamustafic.mersiha@gmail.com>
2024-04-14 23:53:03 +0200

    feat: updates tests.py file

--- tests.py
+++ tests.py
@@ -1,29 +1,20 @@
 import unittest
-from app import extract_data, extract_amount # upload_pdf, detect_report_type,
- 
-# Define class to test the program
-class TestTextExtraction(unittest.TestCase):
-    # Function to test addition function
-    def test_upload_pdf(self):
-        result = addition(2, 2)
-        self.assertEqual(result, 4)
 
-        #./files/test.pdf
-         
-    # Function to test addition function
-    def test_detect_report_type(self):
-        result = subtraction(4, 2)
-        self.assertEqual(result, 2)
-         
-   # Function to test addition function
-    def test_extract_data(self):
-        result = multiplication(2, 3)
-        self.assertEqual(result, 6)
-         
-    # Function to test addition function
-    def test_extract_amount(self):
-        result = division(4, 2)
-        self.assertEqual(result, 2)
- 
+class TestStringMethods(unittest.TestCase):
+
+    def test_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+        # check that s.split fails when the separator is not a string
+        with self.assertRaises(TypeError):
+            s.split(2)
+
 if __name__ == '__main__':
     unittest.main()
