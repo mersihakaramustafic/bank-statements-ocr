712de69cf151389db03513874d8267f5f0cdd0fa
Mersiha Karamustafic <karamustafic.mersiha@gmail.com>
2024-04-21 23:55:54 +0200

    feat: Checking report type

--- app.py
+++ app.py
@@ -56,23 +56,30 @@ def main():
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         # Apply Gaussian blur to reduce noise and improve accuracy
         blurred = cv2.GaussianBlur(gray, (5, 5), 0)
-        #thresholding the image to improve contrast and OCR accuracy
+        # Thresholding the image to improve contrast and OCR accuracy
         thresh = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)[1]
 
-        main_json = {
-            "order_number": extract_data(thresh[c.order_number['start_y']:c.order_number['end_y'], c.order_number['start_x']:c.order_number['end_x']]),
-            "sender": extract_data(thresh[c.sender['start_y']:c.sender['end_y'], c.sender['start_x']:c.sender['end_x']]),
-            "sender_address": extract_data(thresh[c.sender_address['start_y']:c.sender_address['end_y'], c.sender_address['start_x']:c.sender_address['end_x']]),
-            "sender_city": extract_data(thresh[c.sender_city['start_y']:c.sender_city['end_y'], c.sender_city['start_x']:c.sender_city['end_x']]),
-            "receiver": extract_data(thresh[c.receiver['start_y']:c.receiver['end_y'], c.receiver['start_x']:c.receiver['end_x']]),
-            "receiver_address": extract_data(thresh[c.receiver_address['start_y']:c.receiver_address['end_y'], c.receiver_address['start_x']:c.receiver_address['end_x']]),
-            "receiver_city": extract_data(thresh[c.receiver_city['start_y']:c.receiver_city['end_y'], c.receiver_city['start_x']:c.receiver_city['end_x']]),
-            "purpose_of_remittance": extract_data(thresh[c.purpose_of_remittance['start_y']:c.purpose_of_remittance['end_y'], c.purpose_of_remittance['start_x']:c.purpose_of_remittance['end_x']]),
-            "sender_bank_account": extract_data(thresh[c.sender_bank_account['start_y']:c.sender_bank_account['end_y'], c.sender_bank_account['start_x']:c.sender_bank_account['end_x']]),
-            "receiver_bank_account": extract_data(thresh[c.receiver_bank_account['start_y']:c.receiver_bank_account['end_y'], c.receiver_bank_account['start_x']:c.receiver_bank_account['end_x']]),
-            "payment_date": extract_data(thresh[c.payment_date['start_y']:c.payment_date['end_y'], c.payment_date['start_x']:c.payment_date['end_x']]),
-            "amount": extract_amount(thresh[c.amount['start_y']:c.amount['end_y'], c.amount['start_x']:c.amount['end_x']])
-        }
-        return json.dumps(main_json)
+        # Detect report type
+        report_type = detect_report_type(image)
+        print(report_type)
+
+        if(report_type == 'unicredit'):
+            main_json = {
+                "order_number": extract_data(thresh[c.order_number['start_y']:c.order_number['end_y'], c.order_number['start_x']:c.order_number['end_x']]),
+                "sender": extract_data(thresh[c.sender['start_y']:c.sender['end_y'], c.sender['start_x']:c.sender['end_x']]),
+                "sender_address": extract_data(thresh[c.sender_address['start_y']:c.sender_address['end_y'], c.sender_address['start_x']:c.sender_address['end_x']]),
+                "sender_city": extract_data(thresh[c.sender_city['start_y']:c.sender_city['end_y'], c.sender_city['start_x']:c.sender_city['end_x']]),
+                "receiver": extract_data(thresh[c.receiver['start_y']:c.receiver['end_y'], c.receiver['start_x']:c.receiver['end_x']]),
+                "receiver_address": extract_data(thresh[c.receiver_address['start_y']:c.receiver_address['end_y'], c.receiver_address['start_x']:c.receiver_address['end_x']]),
+                "receiver_city": extract_data(thresh[c.receiver_city['start_y']:c.receiver_city['end_y'], c.receiver_city['start_x']:c.receiver_city['end_x']]),
+                "purpose_of_remittance": extract_data(thresh[c.purpose_of_remittance['start_y']:c.purpose_of_remittance['end_y'], c.purpose_of_remittance['start_x']:c.purpose_of_remittance['end_x']]),
+                "sender_bank_account": extract_data(thresh[c.sender_bank_account['start_y']:c.sender_bank_account['end_y'], c.sender_bank_account['start_x']:c.sender_bank_account['end_x']]),
+                "receiver_bank_account": extract_data(thresh[c.receiver_bank_account['start_y']:c.receiver_bank_account['end_y'], c.receiver_bank_account['start_x']:c.receiver_bank_account['end_x']]),
+                "payment_date": extract_data(thresh[c.payment_date['start_y']:c.payment_date['end_y'], c.payment_date['start_x']:c.payment_date['end_x']]),
+                "amount": extract_amount(thresh[c.amount['start_y']:c.amount['end_y'], c.amount['start_x']:c.amount['end_x']])
+            }
+            return json.dumps(main_json)
+        else:
+            return None
     except Exception as e:
         return "An error occurred: " + str(e)
